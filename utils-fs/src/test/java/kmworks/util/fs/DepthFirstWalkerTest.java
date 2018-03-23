236733ec591c61ec068e51d134cfe7708ab5b0e6
Christian P. Lerch <christian.p.lerch@gmail.com>
2018-03-23 18:16:42 +0100

    more/updated tests

--- utils-fs/src/test/java/kmworks/util/fs/DepthFirstWalkerTest.java
+++ utils-fs/src/test/java/kmworks/util/fs/DepthFirstWalkerTest.java
@@ -18,6 +18,9 @@ package kmworks.util.fs;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import kmworks.util.fs.attr.FileAttributes;
+import kmworks.util.lambda.Runnable1;
+import kmworks.util.misc.UUIDGenerator;
 import org.junit.Test;
 
 /**
@@ -34,30 +37,70 @@ public class DepthFirstWalkerTest {
     }
     
     private void printStats(DepthFirstWalker.Statistics stats) {
-        pln("Dirs visited:    " + stats.getDirsVisited());
-        pln("Dirs walked:     " + stats.getDirsWalked());
-        pln("Files visited:   " + stats.getFilesVisited());
+        pln("Dirs visited   : " + stats.getDirsVisited());
+        pln("Dirs walked    : " + stats.getDirsWalked());
+        pln("Dirs processed : " + stats.getDirsProcessed());
+        pln("    processable: " + stats.getDirsProcessable());
+        pln("Files visited  : " + stats.getFilesVisited());
         pln("Files processed: " + stats.getFilesProcessed());
         pln("    processable: " + stats.getFilesProcessable());
-        pln("Timing:         " + stats.getTiming() + " msec");        
+        pln("Timing         : " + stats.getTiming() + " msec");        
     }
     
     @Test
-    public void test_01() {
-        Path rootDir = Paths.get("D:\\mirrors\\EARTH3\\slow_archive\\d-a-c\\assets-docs");
-        DepthFirstWalker walker = new DepthFirstWalker(rootDir);
-        walker.dirsExclude((path, level) -> level == 1 && !path.toString().endsWith("Telefon"));
-        walker.filesExclude((path, level) -> level == 1);
-        walker.walk((Path path, Object ctx) -> {
-            pln(path.toString());
-        }, null);
+    public void testSetFileID() {
+        Path rootDir = Paths.get("C:/_/data/d-a-c/knowledge/IT/TOPICS/SW-PROD");
+        
+        Runnable1<Path> worker = (Path path) -> { 
+            FileAttributes fileAttrs = FileAttributes.of(path);
+            if (!fileAttrs.exists("FILE_ID")) {
+                fileAttrs.set("FILE_ID", UUIDGenerator.generate().toString());
+            }
+            fileAttrs.set("IS_A", path.getParent().getFileName().toString());
+            fileAttrs.close();
+        };
+        
+        DepthFirstWalker<Void> walker = new DepthFirstWalker<>(rootDir);
+        walker.dirsInclude((path, level) -> path.getFileName().toString().matches("\\p{Alnum}.*"));
+        walker.dirsExclude((path, level) -> level > 1);
+        walker.filesExclude((path, level) -> level >= 1);
+
+        walker.walk(worker);
+        
+        printStats(walker.statistics());
+    }
+
+    @Test
+    public void testRenameAttr() {
+        Path rootDir = Paths.get("C:/_/data/d-a-c/knowledge/IT/TOPICS/SW-PROD");
+        String fromAttrName = "WWW_HOME";
+        String toAttrName = "WWW";
+        
+        Runnable1<Path> worker = (Path path) -> { 
+            FileAttributes fileAttrs = FileAttributes.of(path);
+            if (fileAttrs.exists(fromAttrName)) {
+                String homeUrl = fileAttrs.get(fromAttrName);
+                fileAttrs.delete(fromAttrName);
+                fileAttrs.set(toAttrName, homeUrl);
+            }
+            fileAttrs.close();
+        };
+        
+        DepthFirstWalker<Void> walker = new DepthFirstWalker<>(rootDir);
+        walker.dirsInclude((path, level) -> path.getFileName().toString().matches("\\p{Alnum}.*"));
+        walker.dirsExclude((path, level) -> level > 1);
+        walker.filesExclude((path, level) -> level >= 1);
+
+        walker.walk(worker);
+        
         printStats(walker.statistics());
     }
 
     @Test
     public void test_02() {
-        Path rootDir = Paths.get("C:\\Windows");
-        DepthFirstWalker walker = new DepthFirstWalker(rootDir);
+        Path rootDir = Paths.get("C:/Windows");
+        DepthFirstWalker<Void> walker = new DepthFirstWalker(rootDir);
+        walker.dirsExclude((path, level) -> level > 1);
         walker.walk();
         printStats(walker.statistics());
         // Performance: ~ 2.000 files/sec
@@ -65,8 +108,8 @@ public class DepthFirstWalkerTest {
 
     @Test
     public void test_03() {
-        Path rootDir = Paths.get("C:\\_\\dev\\ws");
-        DepthFirstWalker walker = new DepthFirstWalker(rootDir);
+        Path rootDir = Paths.get("C:/_/dev/ws");
+        DepthFirstWalker<Void> walker = new DepthFirstWalker(rootDir);
         walker.walk();
         printStats(walker.statistics());
         // Visited: ~ 28.000 files in 1.500 dirs
