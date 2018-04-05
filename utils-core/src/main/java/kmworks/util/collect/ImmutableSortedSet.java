22e81ae6fc99dad93b916b28e84005afae2b39a5
Christian P. Lerch <christian.p.lerch@gmail.com>
2018-04-05 14:16:38 +0200

    remove not needed methods

--- utils-core/src/main/java/kmworks/util/collect/ImmutableSortedSet.java
+++ utils-core/src/main/java/kmworks/util/collect/ImmutableSortedSet.java
@@ -1,50 +1,46 @@
 /*
  * Copyright (C) 2005-2016 Christian P. Lerch <christian.p.lerch[at]gmail.com>
- *  
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package kmworks.util.collect;
 
 import kmworks.util.iter.SortedIterable;
+
 import java.util.Collection;
 
 /**
- *
  * @author Christian P. Lerch
  */
 public interface ImmutableSortedSet<T> extends SortedIterable<T> {
-  
-   int size();
 
-   boolean isEmpty();
+    T first();
 
-   boolean contains(Object o);
+    T last();
 
-   boolean containsAll(Collection<?> c);
+    int size();
 
-   Object[] toArray();
+    boolean contains(T value);
 
-   <T> T[] toArray(T[] a);
+    default boolean isEmpty() {
+        return size() == 0;
+    }
 
-   T first();
+    ImmutableSortedSet<T> subSet(T fromElement, T toElement);
 
-   T last();
+    ImmutableSortedSet<T> headSet(T toElement);
 
-   ImmutableSortedSet<T> subSet(T fromElement, T toElement);
-
-   ImmutableSortedSet<T> headSet(T toElement);
-
-   ImmutableSortedSet<T> tailSet(T fromElement);
+    ImmutableSortedSet<T> tailSet(T fromElement);
 
 }
