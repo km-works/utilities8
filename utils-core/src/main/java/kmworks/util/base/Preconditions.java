3210e253535bc780a71281596a3c2d39f1af1cf1
Christian P. Lerch <christian.p.lerch@gmail.com>
2018-04-07 09:11:16 +0200

    complement Guava Preconditions

---
+++ utils-core/src/main/java/kmworks/util/base/Preconditions.java
@@ -0,0 +1,45 @@
+package kmworks.util.base;
+
+import kmworks.util.StringPool;
+
+import java.util.Collection;
+
+public final class Preconditions {
+    private Preconditions() {}
+
+    /**
+     * Ensures that a collection passed as a parameter to the calling method is not null and not empty.
+     *
+     * @param collection a collection
+     * @return the non-null and non-empty collection that was validated
+     * @throws NullPointerException if {@code collection} is null
+     * @throws IllegalArgumentException if {@code collection} is empty
+     */
+    //@CanIgnoreReturnValue
+    public static <T, C extends Collection<T>> C checkNotEmpty(C collection) {
+        return checkNotEmpty(collection, null);
+    }
+
+    /**
+     * Ensures that a collection passed as a parameter to the calling method is not null and not empty.
+     *
+     * @param collection a collection
+     * @param errorMessage the exception message to use if the check fails
+     * @return the not null and not empty collection that was validated
+     * @throws NullPointerException if {@code collection} is null
+     * @throws IllegalArgumentException if {@code collection} is empty
+     */
+    //@CanIgnoreReturnValue
+    public static <T, C extends Collection<T>> C checkNotEmpty(C collection, /* @NullableDecl */ String errorMessage) {
+        if (errorMessage == null) errorMessage = StringPool.MUST_NOT_BE_NULL_OR_EMPTY_MSG;
+        if (collection == null) {
+            throw new NullPointerException(errorMessage);
+        }
+        if (collection.isEmpty()) {
+            throw new IllegalArgumentException(errorMessage);
+        }
+        return collection;
+    }
+
+
+}
